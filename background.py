22baf19d5eb3158975f94f85ae42cf5514e744c4
DipodDP <dipoddp@gmail.com>
2023-01-16 20:18:20 +0700

    PyAnywhere always on

--- background.py
+++ background.py
@@ -1,32 +1,36 @@
-import asyncio
-from threading import Thread
+import subprocess
+from subprocess import Popen
 
-from flask import Flask
+from flask import Flask, redirect, url_for
 
-from bot import main, logger
+app = Flask('__name__')
 
-app = Flask('Background')
-
-
-def run():
-    try:
-        asyncio.run(main())
-        return 'Bot is alive!'
-    except (KeyboardInterrupt, SystemExit):
-        logger.warning("Bot stopped!")
-    except RuntimeError as e:
-        print(f'{e}')
-
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+process: Popen | None = None
 
 
 @app.route('/')
 def home():
-    keep_alive()
-    return 'Bot is alive!'
+    global process
+    if process:
+        status = process.poll()
+        if status is None:
+            result = 'alive! :)'
+        else:
+            result = status
+    else:
+        result = 'down! :(. Press <a href="/start">Start</a>'
+    return f'Bot is {result}'
+
+
+@app.route('/start')
+def start():
+    global process
+    if process is None:
+        process = subprocess.Popen(["python", "bot.py"], shell=True)
+        print('Starting...')
+    print(process.poll())
+
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
